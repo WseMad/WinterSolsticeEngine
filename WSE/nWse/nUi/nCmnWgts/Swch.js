1cf04fde03e27ab719b42143768876189c316fa7
WseMad <mgtx460@163.com>
2014-10-07 13:51:47 +0800

    加入tSwch

---
+++ WSE/nWse/nUi/nCmnWgts/Swch.js
@@ -0,0 +1,257 @@
+﻿/*
+*
+*/
+
+
+(function ()
+{
+	//@ 全局对象，同时支持页面主线程，WebWorker线程，和Node.js
+	var i_InNodeJs = ("undefined" == typeof self);
+	var l_Glb = i_InNodeJs ? global : self;
+	
+	//@ 如果本文件已经包含过
+	if (l_Glb.nWse && l_Glb.nWse.nUi && l_Glb.nWse.nUi.nGpuWgts && l_Glb.nWse.nUi.nGpuWgts.tSwch)
+	{
+		//@ 避免重复执行相同的初始化代码
+		return;
+	}
+
+	//@ 包含
+	l_Glb.nWse.stAsynIcld.cFromLib("nWse:nUi/nGpuWgts",
+		[
+			"nWse:nUi/Wgt.js"
+		]
+		,
+		fOnIcld);
+
+function fOnIcld(a_Errs)
+{
+	console.log("Swch.fOnIcld：" + a_Errs);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// using
+
+	var nWse = l_Glb.nWse;
+	var stNumUtil = nWse.stNumUtil;
+	var stStrUtil = nWse.stStrUtil;
+	var stAryUtil = nWse.stAryUtil;
+	var stDomUtil = nWse.stDomUtil;
+	var stCssUtil = nWse.stCssUtil;
+	var tSara = nWse.tSara;
+
+	var tPntIptTrkr = nWse.tPntIptTrkr;
+	var tPntIpt = tPntIptTrkr.tPntIpt;
+	var tPntIptKind = tPntIpt.tKind;
+	var tPntIptTch = tPntIpt.tTch;
+
+	var nUi = nWse.nUi;
+	var tWgt = nUi.tWgt;
+	var stFrmwk = nUi.stFrmwk || null;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 名字空间
+
+	if (! nUi.nCmnWgts)
+	{
+		nWse.fNmspc(nUi,
+			/// 公共控件
+			function nCmnWgts() {});
+	}
+	var nCmnWgts = nUi.nCmnWgts;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 静态变量
+
+	function fRset(a_This)
+	{
+		a_This.d_Blk = null;
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+	var tSwch;
+	(function ()
+	{
+		tSwch = nWse.fClass(nCmnWgts,
+		/// 开关
+		function tSwch()
+		{
+			this.odBase(tSwch).odCall();	// 基类版本
+
+			var l_This = this;
+			fRset(this);
+		}
+		,
+		tWgt
+		,
+		{
+			/// 绑定
+			/// a_Cfg：Object，配置
+			/// {
+			///	c_PutTgt：String，放置目标的HTML元素ID，若不存在则自动创建带有指定ID的<div>，作为c_PutSrc的前一个兄弟节点
+			/// c_PutSrc：String，放置来源的HTML元素ID，必须有效
+			/// }
+			vcBind : function f(a_Cfg)
+			{
+				this.odBase(f).odCall(a_Cfg);	// 基类版本
+
+				var l_This = this;
+				stCssUtil.cAddCssc(l_This.d_PutTgt, "cnWse_tSwch");	// CSS类
+
+				// 创建滑块
+				var l_Blk = document.createElement("div");
+				stCssUtil.cSetCssc(l_Blk, "cnWse_tSwch_Blk");
+				l_This.d_Blk = l_Blk;
+				l_This.d_PutSrc.appendChild(l_Blk);		// 放入来源
+
+				return this;
+			}
+			,
+			/// 解绑
+			vcUbnd : function f()
+			{
+				var l_This = this;
+
+				// 重置
+				fRset(this);
+
+				// 基类版本，最后才调用！
+				this.odBase(f).odCall();
+				return this;
+			}
+			,
+			/// 结束动画
+			vcFnshAnmt : function f()
+			{
+				var l_This = this;
+
+				return this;
+			}
+			,
+			/// 刷新在布局之前
+			vcRfshBefLot : function f()
+			{
+				this.odBase(f).odCall();	// 基类版本
+
+				var l_This = this;
+
+				// 把滑块放入目标
+				l_This.dPutToTgt(l_This.d_Blk);
+
+				return this;
+			}
+			,
+			/// 刷新在布局之后
+			vcRfshAftLot : function f()
+			{
+				this.odBase(f).odCall();	// 基类版本
+
+				var l_This = this;
+
+				// 需要在这里校准滑块的位置
+				return this;
+			}
+			,
+			/// 处理来自支配触点的输入
+			/// a_DmntTchIdx：Number，支配触点索引
+			/// a_DmntTch：Object，支配触点
+			vdHdlIptFromDmntTch : function f(a_Ipt, a_DmntTchIdx, a_DmntTch)
+			{
+				this.odBase(f).odCall(a_Ipt, a_DmntTchIdx, a_DmntTch);	// 基类版本
+
+				var l_This = this;
+
+				if (l_This.dIsTchBgn(a_DmntTch))
+				{
+					a_DmntTch.c_Hdld = true;		// 已处理
+				}
+				else
+				if (l_This.dIsTchLostOrEnd(a_DmntTch))
+				{
+//					if (l_This.dIsTchLost(a_DmntTch))
+//					{
+//						//
+//					}
+//					else
+					if (l_This.dIsTchEnd(a_DmntTch))
+					{
+						l_This.dTgt();					// 切换
+						a_DmntTch.c_Hdld = true;		// 已处理
+					}
+				}
+				return this;
+			}
+			,
+			/// 获得焦点
+			vcGainFoc : function f()
+			{
+				this.odBase(f).odCall();	// 基类版本
+
+				var l_This = this;
+				return this;
+			}
+			,
+			/// 失去焦点
+			vcLoseFoc : function f()
+			{
+				this.odBase(f).odCall();	// 基类版本
+
+				var l_This = this;
+				return this;
+			}
+			,
+			/// 打开？
+			cIsOn : function ()
+			{
+				return stCssUtil.cHasCssc(this.d_PutTgt, "cnWse_tSwch_On");
+			}
+			,
+			/// 切换
+			dTgt : function ()
+			{
+				var l_This = this;
+
+				var l_BlkX;
+				if (l_This.cIsOn())
+				{
+					stCssUtil.cRmvCssc(l_This.d_PutTgt, "cnWse_tSwch_On");
+				//	stCssUtil.cSetPosLt(l_This.d_Blk, 0);
+					l_BlkX = 0;
+				}
+				else
+				{
+					stCssUtil.cAddCssc(l_This.d_PutTgt, "cnWse_tSwch_On");
+				//	stCssUtil.cSetPosLt(l_This.d_Blk, l_This.d_PutTgt.clientWidth - l_This.d_Blk.offsetWidth);
+					l_BlkX = l_This.d_PutTgt.clientWidth - l_This.d_Blk.offsetWidth;	// 注意去掉边框
+				}
+
+				// 动画位置
+				stCssUtil.cAnmt(l_This.d_Blk,
+					{
+						"left": l_BlkX.toString() + "px"
+					},
+					{
+						c_Dur: 0.2,
+						c_fEsn: function (a_Scl)
+						{
+							return a_Scl;
+						}
+					});
+				return this;
+			}
+		}
+		,
+		{
+			//
+		}
+		,
+		false);
+	})();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Over
+
+}
+})();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
